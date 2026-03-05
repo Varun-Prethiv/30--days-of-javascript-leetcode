1cc6055a9193dec1aefaa80d50d581807074229f
Varun Prethiv <varunprethiv7@gmail.com>
2026-03-05 10:17:39 +0530

    Day 2 completed

--- day2-one sum.js
+++ day2-one sum.js
@@ -2,15 +2,31 @@
  * @param {integer} init
  * @return { increment: Function, decrement: Function, reset: Function }
  */
+// var createCounter = function(init) {
+//     let value=init;
+//     return{
+//         increment:()=>++value,
+//         decrement:()=>--value,
+//         reset:()=>value=init
+//     }
+// };
 var createCounter = function(init) {
     let value=init;
-    return{
-        increment:()=>++value,
-        decrement:()=>--value,
-        reset:()=>value=init
+    function increment(){
+        return ++init;
+    }
+    function decrement(){
+        return --init;
+    }
+    function reset(){
+        return init=value;
+    }
+    return {
+        increment,
+        decrement,
+        reset
     }
 };
-
 /**
  * const counter = createCounter(5)
  * counter.increment(); // 6
