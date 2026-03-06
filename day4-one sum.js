ac73c52452ed4687187e1085748a9d785e6c88e0
Varun Prethiv <varunprethiv7@gmail.com>
2026-03-06 09:49:58 +0530

    Day 4 completed

--- day4-one sum.js
+++ day4-one sum.js
@@ -2,30 +2,29 @@
  * @param {string} val
  * @return {Object}
  */
+
 var expect = function(val) {
+
+    // checks if values are equal
     function toBe(n){
-        if(val===n){
-            return true
-        }
-        else{
-            throw new Error("Not Equal")
+        if(val === n){
+            return true;
+        } else {
+            throw new Error("Not Equal");
         }
     }
+
+    // checks if values are not equal
     function notToBe(n){
-         if(val!==n){
-            return true
+        if(val !== n){
+            return true;
+        } else {
+            throw new Error("Equal");
         }
-        else{
-            throw new Error("Equal")
-        }
-    } 
-    return{
+    }
+
+    return {
         toBe,
         notToBe
-    } 
+    }
 };
-
-/**
- * expect(5).toBe(5); // true
- * expect(5).notToBe(5); // throws "Equal"
- */
