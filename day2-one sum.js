20317ed26b61e30dc2e38c8937cdcd4afe5e82f0
Varun Prethiv <varunprethiv7@gmail.com>
2026-03-06 09:57:50 +0530

    Day 2 completed

--- day2-one sum.js
+++ day2-one sum.js
@@ -1,16 +1,16 @@
 var createCounter = function(init) {
-    let value = init;
+    let value = init; // store the current counter value
 
     function increment() {
-        return ++value;
+        return ++value; // increase the value by 1
     }
 
     function decrement() {
-        return --value;
+        return --value; // decrease the value by 1
     }
 
     function reset() {
-        value = init;
+        value = init;   // reset value to the initial number
         return value;
     }
 
